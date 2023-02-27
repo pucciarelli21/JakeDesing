62f6c063f79830e03d38d0dcceb321f420549393
Gaston Pucciarelli <gaston_pucciarelli@hotmail.com>
2023-02-27 20:03:41 -0300

    sobre mi

--- js/main.js
+++ js/main.js
@@ -1,32 +1,6 @@
-/*let getJSONData = function(url){
-    let result = {};
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        return result;
-    });
+const scrollNav = () => {
+  const divNav = document.querySelector(".navTop")
+  let windowsScroll = window.scrollY > 250
+  divNav.classList.toggle("active", windowsScroll)
 }
-*/
-
-const CATEGORIAS = "https://pucciarelli21.github.io/APIS-Pruebas/categorias/categorias.json"
-
-async function getJSONData(url){
-  let peticion = await fetch(url)
-    if (peticion.ok) {
-      let response = await peticion.json()
-      return response
-    }
-}
+window.addEventListener("scroll", scrollNav)
